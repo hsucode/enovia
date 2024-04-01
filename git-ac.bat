fcfc71580006c80ee92ebc458556f7c4eeeb95f8
Your Name <you@example.com>
2024-04-01 10:47:48 +0800

    04/01/2024 Mon

--- git-ac.bat
+++ git-ac.bat
@@ -3,7 +3,7 @@
 Rem xcopy "C:\XUS\200-CODE\Dassault_Systemes\3DEXPERIENCE_XUS_Code\B423\win_b64"\*.* C:\XUS\100-APPS\3DEXPERIENCE_APP\CATApps\BMEDI_B423\CATApps\win_b64    /s /e /c /y /h /r  /d
 
 
-cd /d %~dp0
+cd /d %~dp0book
 
 rem git add V5R27  V5R28 V5R29 V5R30 V5R31 V6R2019x V6R2020x V6R2021x V6R2022x V6R2023x
 git config --global user.email "you@example.com"
